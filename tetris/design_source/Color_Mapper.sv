7c1c565ab3d27019a5027af3895f6b374cbf01a6
Kathryn Thompson <99103786+kyt525@users.noreply.github.com>
2024-12-10 04:07:18 -0600

    in theory, all problems are either syntax, or typing, i am not sure if the math for int/logic and unsigned will cause any error, but in theory, the math should always be correct when Darwx and drwy are in the correct bounds. Also please see if there is a way to make this more efficient, ideally where the math for the different sections only occurs if draw x and draw y apply. like all the math for the tetris sign does not even matter if draw x and draw y are not even within the tetris box, but i don;t know if we can call the functions inside the always comb. NEXT STEPS: add score and time to the right, and next block generation, storage, and display on screen (will probably have to store in an external register), add game logic

--- tetris/design_source/Color_Mapper.sv
+++ tetris/design_source/Color_Mapper.sv
@@ -13,61 +13,128 @@
 //    University of Illinois ECE Department                              --
 //-------------------------------------------------------------------------
 
-
-module  color_mapper ( input  logic [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
-                       output logic [3:0]  Red, Green, Blue );
+//tested BASE BACKGROUND, just the backgrond and ttris board and WORKS
+// module  color_mapper ( input  logic [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
+//                        output logic [3:0]  Red, Green, Blue );
     
-    logic game_on;
-	 
- /* Old Ball: Generated square box by checking if the current pixel is within a square of length
-    2*BallS, centered at (BallX, BallY).  Note that this requires unsigned comparisons.
-	 
-    if ((DrawX >= BallX - Ball_size) &&
-       (DrawX <= BallX + Ball_size) &&
-       (DrawY >= BallY - Ball_size) &&
-       (DrawY <= BallY + Ball_size))
-       )
+//     logic game_on;
 
-     New Ball: Generates (pixelated) circle by using the standard circle formula.  Note that while 
-     this single line is quite powerful descriptively, it causes the synthesis tool to use up three
-     of the 120 available multipliers on the chip!  Since the multiplicants are required to be signed,
-	  we have to first cast them from logic to int (signed by default) before they are multiplied). */
-	  
-    // int DistX, DistY, Size;
-    // assign DistX = DrawX - BallX;
-    // assign DistY = DrawY - BallY;
-    // assign Size = Ball_size;
-  
-    always_comb
-    begin:Ball_on_proc
-        if (DrawX >= 100 && DrawX <= 340) begin
-            game_on = 1'b1;
-        
-        end else begin 
-            game_on = 1'b0;
-        end
-        // if ( (DistX*DistX + DistY*DistY) <= (Size * Size) )
-        //     ball_on = 1'b1;
-        // else 
-        //     ball_on = 1'b0;
-    end 
+//     always_comb
+//     begin:Background_on
+//         if (DrawX >= 100 && DrawX <= 340) begin
+//             game_on = 1'b1;
+//         end
+//         else begin 
+//             game_on = 1'b0;
+//         end
+//         // if ( (DistX*DistX + DistY*DistY) <= (Size * Size) )
+//         //     ball_on = 1'b1;
+//         // else 
+//         //     ball_on = 1'b0;
+//     end 
        
+//     //drawing tetriss sign
+//     always_comb
+//     begin:Draw_backgrond
+//         if ((game_on == 1'b1)) begin 
+//             Red = 4'h0;             //game background
+//             Green = 4'h0;
+//             Blue = 4'h7;
+//         end       
+//         else begin 
+//             Red = 4'h1;             //game background
+//             Green = 4'h0;
+//             Blue = 4'h4;
+//             // this does the gradient from lab 6.2
+//             // Red = 4'hf - DrawX[9:6]; 
+//             // Green = 4'hf - DrawX[9:6];
+//             // Blue = 4'hf - DrawX[9:6];
+//         end      
+//     end 
+// endmodule
+
+
+// MIGHT BE A SOURCE OF ERROR, I FORGOT IF SYSTEM VERILOG IS &&/|| OR &/|
+
+
+// //shorter just for showing the background
+module  background_mapper ( input  logic [9:0]  DrawX, DrawY, 
+                            output logic [3:0]  Red, Green, Blue );
+    
+    // logic game_on;
+    
+    //logic for the tetris sign
+    int tetris_sign_X, tetris_sign_Y;
+    assign tetris_sign_X = 400; //9'h190;     //400 in dec
+    assign tetris_sign_Y = 80;  //9'h50;      //80 in dec
+
+    //not entirely sure if this is correct, but i did this to get the row and color but i didn't know how to
+    //  call a function in the if statement but i needed the color and row but didn't know how to do an if
+    //  statement outside of the always__
+    logic [9:0] loc_in_sign_X, loc_in_sign_Y;
+    assign loc_in_sign_X = DrawX - tetris_sign_X;    //real
+    assign loc_in_sign_Y = DrawY - tetris_sign_Y;    //real
+    
+    logic [9:0] loc_in_sign_X_scaled, loc_in_sign_Y_scaled;
+    assign loc_in_sign_X_scaled = loc_in_sign_X / 3 ;   //caled doen
+    assign loc_in_sign_Y_scaled = loc_in_sign_Y / 3;    //scled down
+
+    logic [9:0] color_index;
+    assign color_index = loc_in_sign_X / 30;        // divide by 10 for letter, divide by 3 for scale
+
+    logic [59:0] char_pixels_ret;
+    tetris_font_rom da_row( 
+        .addr(loc_in_sign_Y_scaled[3:0]),      //this might be wrong, got confused with int v logic and slicing
+        .data(char_pixels_ret));    // get the pixel data
+	 
+    logic [11:0] color_pixels_ret;
+    tetris_font_color da_color(
+        .addr(color_index[2:0]),
+        .data(color_pixels_ret));
+
+
+    //block boundry logic
+    int block_boundry_X, block_boundry_Y;
+    assign block_boundry_X = (DrawX - 100) % 24;    //blck size is 24
+    assign block_boundry_Y = DrawY % 24;
+
     always_comb
-    begin:RGB_Display
-        if ((game_on == 1'b1)) begin 
-            Red = 4'h0;             //game background
+    begin:Draw_backgrond
+        if (DrawX >= 100 && DrawX < 340) begin  
+            Red = 4'h0;                     //game background
             Green = 4'h0;
             Blue = 4'h7;
-        end       
+        end
+
+        //game logic here
+
+        //block boundry logic
+        else if (block_boundry_X == 0 || block_boundry_Y == 0) begin
+            Red = 4'h0;                     // aoutline the blocks
+            Green = 4'h2;
+            Blue = 4'hf;
+        end
+        //outline the game
+        else if (DrawX == 99 || DrawX == 100 || DrawX == 340 || DrawX == 341 || DrawY == 0 || DrawY == 479) begin
+            Red = 4'h0;                     // aoutline the game
+            Green = 4'h8;
+            Blue = 4'hf;
+        end
+        else if ((DrawX >=400 && DrawX <580) && (DrawY >=80 && DrawY < 128) && char_pixels_ret[59-loc_in_sign_X_scaled]) begin 
+            Red = color_pixels_ret[11:8];   //tetris sign
+            Green = color_pixels_ret[7:4];
+            Blue = color_pixels_ret[3:0];
+        end  
         else begin 
-            Red = 4'h1;             //game background
+            Red = 4'h1;                     // background
             Green = 4'h0;
             Blue = 4'h4;
-            // this does the gradient from lab 6.2
-            // Red = 4'hf - DrawX[9:6]; 
-            // Green = 4'hf - DrawX[9:6];
-            // Blue = 4'hf - DrawX[9:6];
-        end      
+        end
     end 
-    
 endmodule
+
+// want the tetris sign to be centered in the 300, 340+60 = 400 -580
+
+// 300m pixels left on the rifht signed
+// 6 letters each w a min of 10 pixels or 60 total
+// try x3 first for tetris of 180 w x 48 h
